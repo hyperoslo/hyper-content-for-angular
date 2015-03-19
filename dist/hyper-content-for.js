47a0d5f80847ea6bc25ce431c22e2f179844edf4
Torgeir Øverland <t0ggah31@gmail.com>
2015-03-19 18:42:15 +0100

    Browserify distribution file

--- dist/hyper-content-for.js
+++ dist/hyper-content-for.js
@@ -1,33 +1,32 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-angular.module("hyperContentFor", [])
-  .value("HYPER_CONTENT_FOR_IDS", { })
+angular.module('hyperContentFor', [])
+  .value('HYPER_CONTENT_FOR_IDS', { })
 
-  .directive("hyperContent", function () {
+  .directive('hyperContent', function() {
     return {
-      scope: { "for": "@" },
+      scope: { 'for': '@' },
 
-      restrict: "E",
+      restrict: 'E',
 
       transclude: true,
 
-      controller: function ($scope, $transclude, HYPER_CONTENT_FOR_IDS) {
-        HYPER_CONTENT_FOR_IDS[$scope["for"]] = $transclude();
+      controller: function($scope, $transclude, HYPER_CONTENT_FOR_IDS) {
+        HYPER_CONTENT_FOR_IDS[$scope['for']] = $transclude();
       }
     };
   })
 
-  .directive("hyperYield", function (HYPER_CONTENT_FOR_IDS) {
+  .directive('hyperYield', function(HYPER_CONTENT_FOR_IDS) {
     return {
-      scope: { to: "@" },
+      scope: { to: '@' },
 
-      restrict: "E",
+      restrict: 'E',
 
-      link: function (scope, elem) {
-        interval = null;
+      link: function(scope, elem) {
 
-        watchFn = function () { return HYPER_CONTENT_FOR_IDS[scope.to]; };
+        watchFn = function() { return HYPER_CONTENT_FOR_IDS[scope.to]; };
 
-        scope.$watch(watchFn, function (newValue) {
+        scope.$watch(watchFn, function(newValue) {
           elem.empty();
           elem.append(newValue);
         });

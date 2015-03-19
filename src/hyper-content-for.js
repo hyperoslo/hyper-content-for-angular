4c62c3e266a97f79ec15fbdeb240349dbdfb5a02
Sindre Moen <sindrenm@gmail.com>
2015-03-19 13:34:49 +0100

    Remove space before function argument lists

--- src/hyper-content-for.js
+++ src/hyper-content-for.js
@@ -1,7 +1,7 @@
 angular.module("hyperContentFor", [])
   .value("HYPER_CONTENT_FOR_IDS", { })
 
-  .directive("hyperContent", function () {
+  .directive("hyperContent", function() {
     return {
       scope: { "for": "@" },
 
@@ -9,24 +9,24 @@ angular.module("hyperContentFor", [])
 
       transclude: true,
 
-      controller: function ($scope, $transclude, HYPER_CONTENT_FOR_IDS) {
+      controller: function($scope, $transclude, HYPER_CONTENT_FOR_IDS) {
         HYPER_CONTENT_FOR_IDS[$scope["for"]] = $transclude();
       }
     };
   })
 
-  .directive("hyperYield", function (HYPER_CONTENT_FOR_IDS) {
+  .directive("hyperYield", function(HYPER_CONTENT_FOR_IDS) {
     return {
       scope: { to: "@" },
 
       restrict: "E",
 
-      link: function (scope, elem) {
+      link: function(scope, elem) {
         interval = null;
 
-        watchFn = function () { return HYPER_CONTENT_FOR_IDS[scope.to]; };
+        watchFn = function() { return HYPER_CONTENT_FOR_IDS[scope.to]; };
 
-        scope.$watch(watchFn, function (newValue) {
+        scope.$watch(watchFn, function(newValue) {
           elem.empty();
           elem.append(newValue);
         });

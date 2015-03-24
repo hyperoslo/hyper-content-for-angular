770d616b5bd11b43a4d9c7a08d0cc1ff53bcea84
Sindre Moen <sindrenm@gmail.com>
2015-03-24 16:22:59 +0100

    Set up Gulp and a build task

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var browserify = require('browserify');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var source = require('vinyl-source-stream');
+var watchify = require('watchify');
+
+var bundler = watchify(browserify(watchify.args));
+
+bundler.add('./src/directives/hyper-content.js');
+bundler.add('./src/directives/hyper-yield.js');
+
+bundler.on('update', bundle);
+bundler.on('log', gutil.log);
+
+function bundle() {
+  return bundler.bundle()
+    .on('error', gutil.log.bind(gutil, 'Bundle Error'))
+    .pipe(source('hyper-content-for.js'))
+    .pipe(gulp.dest('./dist'));
+}
+
+gulp.task('build', bundle);
+
+gulp.task('default', ['build']);
